3234d0d7a64de03d044199ac13bcc0388aa58d7a
maryam <103657696+yaghoobiii@users.noreply.github.com>
2022-04-28 11:59:05 +0430

    Add files via upload

---
+++ againlist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+def all_unique(h):
+    return len(h) == len(set(h))
+
+x=[1,1,2,2,3,3,2,4,5,6]
+y=[1,2,3,4,5]
+all_unique(x) #false
+all_unique(y) #true
+
+
+# In[ ]:
+
+
+from 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
